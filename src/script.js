b837b0ec84c2f780762d4be462f02c76e7c84d1c
Jun <jun55tsuno@gmail.com>
2023-01-18 12:07:29 -0800

    updated

--- src/script.js
+++ src/script.js
@@ -1,8 +1,43 @@
+// navigation
 const menuBtn = document.querySelector(".menu_btn");
 const nav = document.querySelector(".nav");
+// fade-up
+const lagParent = document.querySelectorAll(".lagParent");
+const fadeUpChildren = document.querySelectorAll(".fade-up");
+// fade-in
+const fadeInChildren = document.querySelectorAll(".fade-in");
 
 const toggleMenu = () => {
 	nav.classList.toggle("open");
 };
-
 menuBtn.addEventListener("click", toggleMenu);
+
+// add delay for each item
+lagParent.forEach((el) => {
+	let children = el.children;
+	for (j = 0; j < children.length; j++) {
+		children[j].style.animationDelay = String(j * (1 / children.length) + "s");
+	}
+});
+
+let option = { threshold: 0.4 };
+
+const upObserver = new IntersectionObserver(handleUp, option);
+fadeUpChildren.forEach((el) => {
+	upObserver.observe(el);
+});
+function handleUp(entries) {
+	entries.forEach((el) => {
+		el.isIntersecting && el.target.classList.add("huwa");
+	});
+}
+
+const inObserver = new IntersectionObserver(handleIn, option);
+fadeInChildren.forEach((el) => {
+	inObserver.observe(el);
+});
+function handleIn(entries) {
+	entries.forEach((el) => {
+		el.isIntersecting && el.target.classList.add("huwa");
+	});
+}
